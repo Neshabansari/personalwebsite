fde6309b744d4202c1ac9fbc705ae7073747e12e
Neshab Ansari <neshabalam8008@gmail.com>
2025-02-26 15:12:38 +0530

    my first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed= new Typed(".text", {
+    strings: [" I am a Full Stack Developer" , "video Editor" , "Software Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
